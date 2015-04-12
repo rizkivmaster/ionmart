5304afe667777f7193e450d2ae8c46dd7d1aeaa9
rizkivmaster <rizkivmaster@gmail.com>
2015-04-12 09:41:02 +0700

    add Product.java

---
+++ src/domain/Product.java
@@ -0,0 +1,18 @@
+package com.ionmart.domain
+
+public class Product{
+	private final String _id;
+	private final String _name;
+	private final String _brand;
+
+	public Product(String id, String name, String brand){
+		this._id = id;
+		this._name = name;
+		this._brand = brand;
+	}
+
+	public Product(Product other)
+	{
+		Product(other._id,other._name,other._brand);
+	}
+}
